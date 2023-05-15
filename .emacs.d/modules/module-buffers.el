7b0c33aa9355e625151cbb118b89c9605d53beba
Tracy French <67283114+tracy-french@users.noreply.github.com>
2023-05-14 20:00:04 -0600

    add buffer management bindings

--- .emacs.d/modules/module-buffers.el
+++ .emacs.d/modules/module-buffers.el
@@ -5,8 +5,7 @@
   :defer t
   :general
   (:states 'normal
-           "SPC b" '(:ignore t :which-key "buffers")
-           "SPC b i" 'ibuffer))
+           "SPC b i" '(ibuffer :wk "ibuffer")))
 
 (use-package ibuffer-vc
   :defer t
@@ -20,5 +19,16 @@
   :general
   (:states 'normal
            "SPC b b" 'consult-buffer))
+
+(use-package general
+  :general
+  (:states 'normal
+           "SPC b" '(:ignore t :which-key "buffers")
+           "SPC b k" '(kill-current-buffer :wk "kill current buffer")
+           "SPC b K" '(kill-buffer :wk "kill buffer")
+           "SPC b p" '(previous-buffer :wk "previous buffer")
+           "SPC b n" '(next-buffer :wk "next buffer")
+           "SPC b s" '(save-buffer :wk "save buffer")
+           "SPC b S" '(save-some-buffers :wk "save some buffers")))
 
 (provide 'module-buffers)
